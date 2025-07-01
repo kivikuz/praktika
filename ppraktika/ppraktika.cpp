e3e02a6323dd434ef01206399a0293180acc9ff7
kivikuz <kivikuz@yandex.ru>
2025-07-01 03:17:37 +0300

    Функция сортировки выбором

--- ppraktika/ppraktika.cpp
+++ ppraktika/ppraktika.cpp
@@ -51,3 +51,19 @@ int InputArray() {
     }
     return input;
 }
+
+void selectionSort(int a[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++) {
+            if (a[j] < a[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            int temp = a[i];
+            a[i] = a[minIndex];
+            a[minIndex] = temp;
+        }
+    }
+}
